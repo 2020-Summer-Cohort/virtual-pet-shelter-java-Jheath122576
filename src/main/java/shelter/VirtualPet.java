9c7cc9ec1c62ff95e8b5eee97bd98a27b6087903
Ben Williams <DeanPBR@gmail.com>
2020-05-29 13:31:39 -0400

    VirtualPet wireframe.

---
+++ src/main/java/shelter/VirtualPet.java
@@ -0,0 +1,27 @@
+package shelter;
+
+public class VirtualPet {
+        /*
+         * Things this class knows:
+         *
+         * hunger
+         * thirst
+         * boredom
+         * name
+         * description
+         *
+         */
+    //constructor goes here.
+
+
+    /*
+     * eat() -> hunger
+     * drink() -> thirst
+     * play() -> boredom
+     *
+     * tick() -> hunger, thirst, & boredom
+     *
+     */
+
+    // getters go here
+}
